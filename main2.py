8f3a982fd4b872e846880976961688ae3675dbf5
Tumani1337 <idi_suda06@mail.ru>
2025-03-07 15:09:32 +0300

    fix: check_triangle_exists

--- main2.py
+++ main2.py
@@ -2,11 +2,8 @@ import math
 
 def check_triangle_exists(a:float ,b: float,c: float) -> None:
 
-    while ((a + b > c) and (a + c > b) and (b + c > a)):
-        print('Треугольник не существует')
-        a = float(input('a>> '))
-        b = float(input('b>> '))
-        c = float(input('c>> '))
+    if not ((a + b > c) and (a + c > b) and (b + c > a)):
+        raise ValueError('Треугольник не существует')
 
 def is_positive(a:float ,b: float,c: float) -> None:
 
