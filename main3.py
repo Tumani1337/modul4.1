ff574a51e474218f7c0b42d28ab7e3682f91d5b0
Tumani1337 <idi_suda06@mail.ru>
2025-03-07 15:21:54 +0300

    feat: input_choice

--- main3.py
+++ main3.py
@@ -4,3 +4,13 @@ def check_correct_choice(choice: int) -> int:
         choice = int(input('>>>'))
 
     return choice
+
+def input_choice() -> int:
+
+    print("Выберите опцию:")
+    print("1. Конвертировать Цельсий в Фаренгейт")
+    print("2. Конвертировать Фаренгейт в Цельсий")
+    choice = int(input('>>>'))
+    choice = check_correct_choice(choice)
+
+    return choice
